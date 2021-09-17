9dc80eae6d9cd8aa565c94226bff7f8293984709
Pranav-b-7 <pranav.bhaskaran@opsmx.io>
2021-09-17 15:44:12 +0530

    @Component annotation added

--- gate-core/src/main/java/com/opsmx/spinnaker/gate/audit/LoginAttemptListener.java
+++ gate-core/src/main/java/com/opsmx/spinnaker/gate/audit/LoginAttemptListener.java
@@ -21,8 +21,10 @@ import org.springframework.boot.actuate.audit.AuditEvent;
 import org.springframework.boot.actuate.audit.listener.AuditApplicationEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 public class LoginAttemptListener {
 
   @EventListener
