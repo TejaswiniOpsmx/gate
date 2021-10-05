21abf312c814099cb5336f3ac32573d3a9c930e7
rahul-chekuri <rahulgandhi.chekuri@opsmx.io>
2021-10-05 16:54:57 +0530

    OP-7604: Handling exception cases

--- gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/OpsmxOesController.groovy
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/OpsmxOesController.groovy
@@ -332,7 +332,7 @@ class OpsmxOesController {
     }.call() as okhttp3.Response
 
     if (!obj.isSuccessful()) {
-      throw OesRequestException(response.body().string())
+      throw OesRequestException(obj.body().string())
     } else{
       return obj.body()?.string() ?: "Unknown reason: " + obj.code() as Object
     }
@@ -359,7 +359,7 @@ class OpsmxOesController {
 	  }.call() as okhttp3.Response
 
     if (!obj.isSuccessful()) {
-      throw OesRequestException(response.body().string())
+      throw OesRequestException(obj.body().string())
     } else{
       return obj.body()?.string() ?: "Unknown reason: " + obj.code() as Object
     }
