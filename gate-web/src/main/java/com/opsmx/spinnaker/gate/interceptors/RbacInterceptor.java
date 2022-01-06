ab4a5175751fec11e12b8b7772a9c2b9a29fc8f9
Pranav-b-7 <pranav.bhaskaran@opsmx.io>
2022-01-06 18:35:44 +0530

    added x-spinnaker-user header

--- gate-web/src/main/java/com/opsmx/spinnaker/gate/interceptors/RbacInterceptor.java
+++ gate-web/src/main/java/com/opsmx/spinnaker/gate/interceptors/RbacInterceptor.java
@@ -41,7 +41,7 @@ public class RbacInterceptor implements HandlerInterceptor {
     log.info("username : {}", userName);
     try {
       ResponseEntity<Object> responseEntity =
-          oesAuthorizationService.authorizeUser(userName, "APP", "VIEW");
+          oesAuthorizationService.authorizeUser(userName, "APP", "VIEW", userName);
       log.info("response : {}", responseEntity.getBody());
     } catch (Exception e) {
       log.error("Exception occurred while authorizing the user : {}", e);
