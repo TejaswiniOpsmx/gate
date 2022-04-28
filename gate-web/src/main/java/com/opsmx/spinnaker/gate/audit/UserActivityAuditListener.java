52607e9e8310b85cc9602b4a0b2d935f3c84c20e
rahul-chekuri <rahulgandhi.chekuri@opsmx.io>
2022-04-28 14:49:42 +0530

    OP-13330: lazy loading handler to avoid

--- gate-web/src/main/java/com/opsmx/spinnaker/gate/audit/UserActivityAuditListener.java
+++ gate-web/src/main/java/com/opsmx/spinnaker/gate/audit/UserActivityAuditListener.java
@@ -24,6 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
@@ -32,6 +33,7 @@ import org.springframework.web.context.support.ServletRequestHandledEvent;
 @Slf4j
 @Component
 @EnableAsync
+@Lazy
 public class UserActivityAuditListener implements ApplicationListener {
 
   @Autowired private AuditHandler auditHandler;
